1a15cc4d44f2ee1aada34302265fd208685a5b0c
mcarcelen <mcarcelen@uees.edu.ec>
2024-06-05 15:43:06 -0500

    Imagenes agredadas

--- deberAngular/src/app/app.component.ts
+++ deberAngular/src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { ProductService } from './services/product.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Product } from './interfaces/product';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductComponent, HttpClientModule],
+  providers: [ProductService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'deberAngular';
+  productos: Product[] = [];
+  
+  constructor(private productService: ProductService){
+    productService.obtenerDatos().subscribe(respuesta => {
+      this.productos = respuesta as Array<Product>
+    });
+  }
 }
